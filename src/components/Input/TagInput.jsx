ceeac67a0a8e3b68a0b9e6e7725814462eea633b
AbdulazizRus <coderdim@gmail.com>
2024-06-13 10:23:53 +0500

    aasa

--- src/components/Input/TagInput.jsx
+++ src/components/Input/TagInput.jsx
@@ -1,7 +1,16 @@
+import {MyAdd, MdClose} from 'react-icons/md'
 
-const TagInput = () => {
+const TagInput = ({tags, setTags}) => {
   return (
-    <div>TagInput</div>
+    <div>
+        <div className="flex items-center gap-4 mt-3">
+           <input type="text" className="" placeholder="Add tags" />
+
+           <button className="">
+               <MyAdd className="text-2xl text-blue-700 hover:text-white"/>    
+           </button> 
+        </div>
+    </div>
   )
 }
 
